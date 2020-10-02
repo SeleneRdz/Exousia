e036ffd68efa9e720e3b5f3b0ff11b1467444856
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2020-10-02 16:53:59 -0500

    version con las configuraciones

---
+++ Assets/Scripts/Scr_MainCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_MainCamera : MonoBehaviour
+{
+    public float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         x += Time.deltaTime * Input.GetAxisRaw("Mouse Y") * 100    ;
+         transform.rotation = Quaternion.Euler(x,0,0);
+         transform.position = new Vector3(0,x/10,(x/10)-10);
+         
+         
+    }
+
+}
---
+++ Assets/Scripts/Scr_Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Player : MonoBehaviour
+{
+  public int vida;
+  public Vector3 posicion;
+  public float velocidad; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+      posicion = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+      posicion = posicion.normalized;
+      transform.position += posicion * velocidad * Time.deltaTime;
+    }
+}
