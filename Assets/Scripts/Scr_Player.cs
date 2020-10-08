4c013fd64aad5763ed2a76e602c27276141b42ea
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2020-10-07 19:03:53 -0500

    version prematura exousia

--- Assets/Scripts/Scr_Player.cs
+++ Assets/Scripts/Scr_Player.cs
@@ -7,18 +7,31 @@ public class Scr_Player : MonoBehaviour
   public int vida;
   public Vector3 posicion;
   public float velocidad; 
+  Animator personajeAnim;
 
     // Start is called before the first frame update
     void Start()
     {
+      personajeAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      float MoverLateral = Input.GetAxisRaw("Horizontal");
+      float MoverHorizontal = Input.GetAxisRaw("Vertical");
       
-      posicion = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+      posicion = new Vector3(MoverLateral, MoverHorizontal, 0);
       posicion = posicion.normalized;
       transform.position += posicion * velocidad * Time.deltaTime;
+      personajeAnim.SetFloat("VelMovimiento", Mathf.Abs(MoverLateral));
+
+
+      if(MoverLateral <= -1){
+        transform.localScale = new Vector3(-1, 1, 1);
+      }
+      if(MoverLateral >= 1){
+        transform.localScale = new Vector3(1, 1, 1);
+      }
     }
 }
